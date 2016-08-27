440f936d85eb0838e5b2fafc36bc5b3729847d59
Nimurai <k.harui.philp@gmail.com>
2016-08-28 02:27:44 +1000

    Integration?!?!
    Attempted to lin Fertile and Plant together. With colliders.... perhaps
    it worked, I'm not too sure.
    If it has then consider the maths complete for the time being.

--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -10,6 +10,7 @@ public enum GrowthStatus
 
 public class Plant : MonoBehaviour
 {
+    private Plant _plant;
     [SerializeField]
     private GrowthStatus _currentStatus;
 
@@ -17,20 +18,44 @@ public class Plant : MonoBehaviour
     private int _maturityTime; // How many seasons it takes to grow
 
     [SerializeField]
-    private int _totalPercentageGrown; // This will determine how much is harvestable
+    private float _totalPercentageGrown; // This will determine how much is harvestable
 
     [SerializeField]
-    private int _fertilityModifier; //The external affects acting upon the plant
+    private float _fertilityModifier; //The external affects acting upon the plant
     //This is presumable a value from 0-(1 for example) i.e. 0.05 is +5% fertility
 
     [SerializeField]
-    private int _harvestModifier; //The intrinsic worth that the plant is "worth" economically (i.e. with respect to the scoring system
-    //_harvestModifier is going to need some mad tinkering depending on what value we want to put on plants
-    //PS... for the mean time _harvestModifier is presumed to be some percentage above 100%.... i.e. 1.05 for +5% _harvest bonus
-    //NEED TO DO!!! Plant Values
+    private int _harvestModifier; 
+    //The intrinsic worth that the plant is "worth" economically (i.e. with respect to the scoring system
+//_harvestModifier is going to need some mad tinkering depending on what value we want to put on plants
+//PS... for the mean time _harvestModifier is presumed to be some percentage above 100%.... i.e. 1.05 for +5% _harvest bonus
+//NEED TO DO!!! Plant Values
 
+    [SerializeField]
+    private int _waterRequirement;
+
+
+    private Fertile _occupiedTile;
+
+    public void Start()
+    {
+
+        var _raycast = Physics2D.Raycast(Vector2.zero, Vector2.zero);
+        if (_raycast.collider != null)
+        {
+            _occupiedTile = _raycast.transform.GetComponent<Fertile>();
+            _occupiedTile.toggleCropRotation();
+            _occupiedTile.setCurrentCrop(_plant.name);
+        }
+        setFertilityModifier(_occupiedTile);
+        checkFerTileWaterSupply();
+    }
     
-    public int HarvestPlant() {
+    private void setFertilityModifier(Fertile _ferTile)
+    {
+        _fertilityModifier = _ferTile.getFertilityModifier();
+    }
+    public float HarvestPlant() {
         //TODO: Fix the return amount (I think I fixed it.....)
 
         return _totalPercentageGrown * _maturityTime / (_harvestModifier - _fertilityModifier); // Probably some base multiplier and specific multipler per plant type?
@@ -44,8 +69,8 @@ public class Plant : MonoBehaviour
 
         return _totalPercentageGrown == 100;
     }
-    public void checkFerTileFertility()
+    public void checkFerTileWaterSupply()
     {
-
+        _fertilityModifier *= _occupiedTile.getFertileWaterPercentage() / _waterRequirement;
     }
 }
--- Assets/Scripts/Tiles/Fertile.cs
+++ Assets/Scripts/Tiles/Fertile.cs
@@ -7,48 +7,101 @@ public class Fertile : Tile
     private float _baseFetilityLevel;
 
     [SerializeField]
-    private double _fertilityModifiers;
+    private float _fertilityModifiers;
 
     [SerializeField]
-    public int _burntField; //1 or 0 not bool because need for math (less if statements for you)
+    public float _burntStatus; //1 or 0 not bool because need for math (less if statements for you)
 
     [SerializeField]
-    public int _hoeStatus; //1 or 0 not bool because need for math (less if statements for you)
+    public float _hoeStatus; //1 or 0 not bool because need for math (less if statements for you)
 
     [SerializeField]
-    public int _ploughStatus;//1 or 0 not bool because need for math (less if statements for you)
+    public float _ploughStatus;//1 or 0 not bool because need for math (less if statements for you)
 
     [SerializeField]
-    public int _irrigationStatus;//1 or 0 not bool because need for math (less if statements for you)
+    public float _irrigationStatus;//1 or 0 not bool because need for math (less if statements for you)
 
     [SerializeField]
-    public int _aqueductStatus;//1 or 0 not bool because need for math (less if statements for you)
+    public float _aqueductStatus;//1 or 0 not bool because need for math (less if statements for you)
+
+    [SerializeField]
+    public float _cropRotationStatus = 1 ;//1 or 0 not bool because need for math (less if statements for you)
+
+    [SerializeField]
+    private string _previousCrop;
+
+    [SerializeField]
+    private string _currentCrop;
 
     private WaterChannel _waterSource;
 
     private int _waterLevel;
 
-    public enum FerTileStatus {
-        hoeStatus,
-        burntStatus,
-        ploughStatus,
-        irrigationStatus,
-        aqueductStatus,
-    }
     private void Start() {
         CheckforWaterSource();
-        _fertilityModifiers = _burntField * 0.25 + _hoeStatus * 0.05 + _ploughStatus * 0.10 + _irrigationStatus * 0.15 + _aqueductStatus * 0.20;
-        string  test = "hoeStatus";
-        int value = 1;
-        changeModifiers(test, value);
+        checkCropRotation();
+        _fertilityModifiers = (_burntStatus * 0.25f + _hoeStatus * 0.05f + _ploughStatus * 0.10f + _irrigationStatus * 0.15f + _aqueductStatus * 0.20f)*_cropRotationStatus;
+       // string  test = "hoeStatus";
+       // int value = 1;
+        //changeModifiers(test, value);
+        //VALUES NEED INVESTIGATING
     }
-    public void changeModifiers(string status, int value){
+
+    public float getFertilityModifier()
+    {
+        return _fertilityModifiers;
+    }
+    public void changeModifiers(string status, int value){ 
+        //external editting of modifiers (tech tree stuff so i'm clueless as to where exactly you want this)
         if (status.Equals("hoeStatus"))
         {
             _hoeStatus = 1;
-            Debug.LogError("hoeStatus is 1");
+            // Debug.LogError("hoeStatus is 1");
+        }
+        else if (status.Equals("ploughStatus"))
+        {
+            _ploughStatus = 1;
+        }
+        else if (status.Equals("irrigationStatus"))
+        {
+            _irrigationStatus = 1;
+        }
+        else if (status.Equals("aqueductStatus"))
+        {
+            _aqueductStatus = 1;
+        }
+        else if (status.Equals("burntStatus"))
+        {
+            _burntStatus = 1;
         }
     }
+    //Here's the if statements you know and love
+    
+        public void toggleCropRotation()
+    {
+        _previousCrop = _currentCrop;
+    }
+
+    public void setCurrentCrop(string cropName)
+    {
+        _currentCrop = cropName;
+    }
+
+    private void checkCropRotation()
+    {
+        if (_previousCrop != null)
+        {
+            if (_currentCrop.Equals(_previousCrop))
+            {
+                _cropRotationStatus *= 0.9f;
+            }
+            else
+            {
+                _cropRotationStatus = 1;
+            }
+        }
+    }
+    
     private void CheckforWaterSource() {
         // Up
         var raycast = Physics2D.Raycast(transform.position + (transform.up * 1.5f), Vector2.zero);
