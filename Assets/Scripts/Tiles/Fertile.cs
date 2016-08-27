899382d586f4afdccdf5e675f90e59f1c24e7a9a
Curtis Fulton <Curtis33210@hotmail.com>
2016-08-27 22:44:22 +1000

    Started on Water Channels and shit

--- Assets/Scripts/Tiles/Fertile.cs
+++ Assets/Scripts/Tiles/Fertile.cs
@@ -8,10 +8,28 @@ public class Fertile : Tile
 
     [SerializeField]
     private float fertilityModifiers;
-    
-    private Plant _activePlant;
 
-    public bool HasPlant { get { return _activePlant != null; } }
+    private WaterChannel _waterSource;
 
+    private int _waterLevel;
     
+    private void Start() {
+        CheckforWaterSource();
+    }
+
+    private void CheckforWaterSource() {
+        // Up
+        var raycast = Physics2D.Raycast(transform.position + (transform.up * 1.5f), Vector2.zero);
+
+        if (raycast.collider != null)
+            _waterSource = raycast.transform.GetComponent<WaterChannel>();
+
+        // Down
+        raycast = Physics2D.Raycast(transform.position - (transform.up / 2), Vector2.zero);
+
+        if (raycast.collider != null)
+            _waterSource = raycast.transform.GetComponent<WaterChannel>();
+
+        Debug.Log(_waterSource.name);
+    }
 }
