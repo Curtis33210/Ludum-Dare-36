69196f89cd5de24aeebbd54b6b0a678812245991
Curtis Fulton <Curtis33210@hotmail.com>
2016-08-27 21:32:46 +1000

    Added basic class base types

---
+++ Assets/Scripts/Plants/Plant.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public enum GrowthStatus
+{
+    Empty,
+    Preparing,
+    Growing,
+    Harvesting,
+}
+
+public class Plant : MonoBehaviour
+{
+    [SerializeField]
+    private GrowthStatus _currentStatus;
+
+    [SerializeField]
+    private Sprite _displaySprite;
+
+
+}
---
+++ Assets/Scripts/WaterChannel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class WaterChannel : MonoBehaviour
+{
+    private static int _totalPercentage; 
+
+    [SerializeField]
+    [Range(0, 100)]
+    private int _percentageOfWaterSource;
+
+    private void Awake() {
+        _totalPercentage = 0;
+    }
+
+    private void Start() {
+        _totalPercentage += _percentageOfWaterSource;
+
+        if (_totalPercentage > 100) {
+            Debug.LogError("The total percentage of Water entries is great than 100%");
+        }
+    }
+}
