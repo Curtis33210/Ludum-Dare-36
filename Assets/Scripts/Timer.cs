f7cf7060f84540c1c607932e4a0f1e6ae266be9b
Curtis Fulton <Curtis33210@hotmail.com>
2016-08-28 14:09:18 +1000

    Added new Scenes and a way to go between them

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    private int _waitTime;
+
+    [SerializeField]
+    private GameObject _targetGameObject;
+
+    private void OnEnable() {
+        StartCoroutine("StartTimer");
+    }
+
+    private IEnumerator StartTimer() {
+        yield return new WaitForSeconds(_waitTime);
+
+        _targetGameObject.SetActive(true);
+    }
+}
