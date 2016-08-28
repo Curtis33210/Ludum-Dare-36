e2e3a739affe8dfba5ae01fc6dc0b7d046d22100
Curtis Fulton <Curtis33210@hotmail.com>
2016-08-28 10:47:29 +1000

    Added Ghost object planting

---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class FollowMouse : MonoBehaviour
+{
+    public bool OverValidPosition { get; private set; }
+
+    private void Update() {
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        transform.position = new Vector2(Mathf.Floor(mousePos.x), Mathf.Floor(mousePos.y));
+
+        if (Input.GetKeyDown(KeyCode.R)) {
+            transform.Rotate(0, 0, -90);
+        }
+
+        if (IsValidPosition()) {
+            GetComponent<SpriteRenderer>().color = new Color(0, 1, 0, 0.5f);
+            OverValidPosition = true;
+        } else {
+            GetComponent<SpriteRenderer>().color = new Color(1, 0, 0, 0.5f);
+            OverValidPosition = false;
+        }
+    }
+
+    private bool IsValidPosition() {
+        var raycast = Physics2D.Raycast(transform.position + new Vector3(0.5f, 0.5f), Vector2.zero, 1, 1 << LayerMask.NameToLayer("Fertile"));
+        
+        if (raycast.collider == null || raycast.transform.GetComponent<Fertile>() == null)
+            return false;
+
+        return true;
+    }
+}
--- Assets/Scripts/PlantPlacer.cs
+++ Assets/Scripts/PlantPlacer.cs
@@ -1,7 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class PlantPlacer : MonoBehaviour
 {
-	
+    [SerializeField]
+    private Transform _hotbarTransform;
+
+    [SerializeField]
+    private GameObject _hotbarButtonPrefab;
+
+    [SerializeField]
+    private GameObject[] _plantPrefabs;
+
+    [SerializeField]
+    private GameObject _ghostPrefab;
+
+    [SerializeField]
+    private GameObject _ghostObject;
+
+    private void Awake() {
+        for (int i = 0; i < _plantPrefabs.Length; i++) {
+            var newHotbarButton = Instantiate(_hotbarButtonPrefab);
+            newHotbarButton.name = "HotKey_" + i;
+            newHotbarButton.transform.SetParent(_hotbarTransform);
+            newHotbarButton.GetComponent<Image>().sprite = _plantPrefabs[i].GetComponent<SpriteRenderer>().sprite;
+            newHotbarButton.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+            newHotbarButton.GetComponentInChildren<Text>().text = (i + 1).ToString();
+
+            var index = i;
+
+            newHotbarButton.GetComponent<Button>().onClick.AddListener(() => {
+                OnButtonPressed(index);
+            });
+        }
+
+        _ghostObject = Instantiate(_ghostPrefab);
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(1)) {
+            _ghostObject.SetActive(false);
+        }
+    }
+
+    public void OnButtonPressed(int buttonIndex) {
+        _ghostObject.GetComponent<SpriteRenderer>().sprite = _plantPrefabs[buttonIndex].GetComponent<SpriteRenderer>().sprite;
+
+        _ghostObject.SetActive(true);
+    }
 }
